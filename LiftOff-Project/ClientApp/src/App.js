2b3a21896b2215deba5eb099e03f3b84a6d2e4d4
priya12845 <priya12845@gmail.com>
2023-01-05 10:09:36 -0600

    Tried bootstrap

--- LiftOff-Project/ClientApp/src/App.js
+++ LiftOff-Project/ClientApp/src/App.js
@@ -4,19 +4,29 @@ import AppRoutes from './AppRoutes';
 import { Layout } from './components/Layout';
 import './custom.css';
 
+
+//import SearchPage from './components/SearchPage';
+
 export default class App extends Component {
   static displayName = App.name;
 
   render() {
-    return (
-      <Layout>
-        <Routes>
-          {AppRoutes.map((route, index) => {
-            const { element, ...rest } = route;
-            return <Route key={index} {...rest} element={element} />;
-          })}
-        </Routes>
-      </Layout>
+      return (
+          <>
+              <div>
+                  Hello
+                </div>          
+          
+            <Layout>
+                <Routes>
+                    {AppRoutes.map((route, index) => {
+                    const { element, ...rest } = route;
+                    return <Route key={index} {...rest} element={element} />;
+                    })}
+                </Routes>
+                  </Layout>
+          
+          </>
     );
   }
 }
---
+++ LiftOff-Project/ClientApp/src/components/SearchPage.js
@@ -0,0 +1,21 @@
+﻿import React, { Component } from 'react'
+ 
+
+export class SearchPage extends Component
+{
+    render() { 
+    return
+    (
+        <div className="Search">
+            <div className="SearchInputs">
+                <input type="text" />
+                <div className="SearchIcon">
+                </div>
+            </div>
+            <div className="dataResult">
+
+            </div>
+        </div>
+        }
+    )
+}
