b483544837648e9ac497b197bf05b3dea2e7fee6
Jordan <jordan_anthony11@aol.com>
2023-01-07 23:29:42 -0600

    Made updates to Create Account form and added some CSS

--- LiftOff-Project/ClientApp/src/components/CreateAccountsPage/CreateAccount.js
+++ LiftOff-Project/ClientApp/src/components/CreateAccountsPage/CreateAccount.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
-import { Header } from "./Header";
-import { Registration } from './Registration';
-import './CreateAccount.css';
+import { RegistrationForm } from './RegistrationForm';
+import css from './CreateAccount.module.css';
 
 export class CreateAccount extends Component {
   static displayName = CreateAccount.name;
 
   render() {
     return (
-      <div class="format">
-        <Header />
-        <Registration />
+      <div className={css.format}>
+        <RegistrationForm />
       </div>
     );
   }
