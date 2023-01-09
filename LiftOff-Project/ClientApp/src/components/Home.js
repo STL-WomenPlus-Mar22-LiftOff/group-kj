11892c1265e2d9013119096a53cfccb075cbf29e
Annalyssa Stakes <annalyssastakes@Annalyssas-Air.attlocal.net>
2023-01-08 18:27:19 -0600

    created search form, set up home page so most things are on grid, edited some CSS

--- LiftOff-Project/ClientApp/src/components/Home.js
+++ LiftOff-Project/ClientApp/src/components/Home.js
@@ -1,41 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Home.css';
+import { SearchForm } from "./SearchForm";
 
-export class Home extends Component {
+export class Home extends React.Component {
   static displayName = Home.name;
 
   render() {
     return (
         <div class="col-xs-12 col-sm-6 col-md-8">
-        
-        <h1>NEXTwatch</h1><br/>
+            <h1 class="h1">NEXTwatch</h1><br />
+            <h2 class="h2">One search for all your streaming services. Find your next favorite movie here!</h2><br />
 
-        <h2>One search for all your streaming services. Find your next favorite movie here!</h2><br />
+            <ul class="gallery">
+                <li>netflix</li>
+                <li>hulu</li>
+                <li>disney +</li>
+                <li>hbo max</li>
+                <li>amazon prime</li>
+            </ul>
 
             <a href="/create-account">Sign up</a><br /><br />
 
-            <form>
+            <SearchForm />
 
-                <label for="genre">Choose a genre:</label>
-                <select name="genre" id="genre">
-                    <option value="action">Action</option>
-                    <option value="romance">Romance</option>
-                    <option value="comedy">Comedy</option>
-                    <option value="horror">Horror</option>
-                </select> <br /><br />
-
-                <label for="rating">Choose a rating:</label>
-                <select name="rating" id="geratingnre">
-                    <option value="fourOrMore">Four or more stars</option>
-                    <option value="threeOrMore">Three or more stars</option>
-                    <option value="twoOrMore">Two or more stars</option>
-                    <option value="oneOrMore">One or more stars</option>
-                </select>
-                <br /><br />
-
-                <input type="submit" />
-
-        </form>
+            <img class="lowerleft" src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg" />
+            <p class="lowerleft">Credit: This product uses the TMDB API but is not endorsed or certified by TMDB.</p>
+            
 
         </div>
     );
---
+++ LiftOff-Project/ClientApp/src/components/SearchForm.js
@@ -0,0 +1,21 @@
+﻿import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+import Button from 'react-bootstrap/Button';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './SearchForm.css';
+
+export class SearchForm extends React.Component {
+    render() {
+      return (
+          <form class="right">
+              <label>
+                  Name:
+              <input type="text" name="name" />
+              </label>
+              <input type="submit" value="Submit" />
+          </form>
+        );
+    }
+}
