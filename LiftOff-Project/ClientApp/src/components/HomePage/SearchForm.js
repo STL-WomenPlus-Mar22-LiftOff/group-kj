d72cd376a2c393af8b405bea50f35c5dc046c715
Annalyssa Stakes <annalyssastakes@Annalyssas-Air.attlocal.net>
2023-01-10 17:03:07 -0600

    pseudocoded search form

--- LiftOff-Project/ClientApp/src/components/HomePage/SearchForm.js
+++ LiftOff-Project/ClientApp/src/components/HomePage/SearchForm.js
@@ -1,12 +1,10 @@
-﻿import { useForm } from "react-hook-form";
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from "yup";
-import Button from 'react-bootstrap/Button';
+﻿import Button from 'react-bootstrap/Button';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import css from './SearchForm.module.css';
 
 export class SearchForm extends React.Component {
+
     render() {
       return (
           <form className={css.right}>
