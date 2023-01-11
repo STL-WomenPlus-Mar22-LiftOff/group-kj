fa9f58d499cba97e46d5a9fda2ed95ad5b35319b
Annalyssa Stakes <annalyssastakes@Annalyssas-Air.attlocal.net>
2023-01-11 13:00:08 -0600

    created some basic input fields to act as placeholders for now

--- LiftOff-Project/ClientApp/src/components/HomePage/SearchForm.js
+++ LiftOff-Project/ClientApp/src/components/HomePage/SearchForm.js
@@ -6,17 +6,26 @@ import css from './SearchForm.module.css';
 export class SearchForm extends React.Component {
 
     render() {
-      return (
-          <form className={css.right}>
-              <label>
-                  
-              <input type="text" name="name" />
-              </label>
-              <input type="submit" value="Submit" />
-          </form>
+        return (
+            <form className={css.right}>
+                <label> Genre:
+              <input type="text" name="genre" />
+                </label>
+                <label> Rating
+              <input type="text" name="rating" />
+                </label>
+                <label> Year Released:
+              <input type="text" name="yearreleased" />
+                </label>
+                <label> Streaming Services:
+              <input type="text" name="genre" />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
         );
     }
 }
+
 //need to create a search form that allows for searching the database
 //search form will need to include genre (drop down list), rating (drop down list), year released (text boxes for range), and streaming services (check boxes).
 
