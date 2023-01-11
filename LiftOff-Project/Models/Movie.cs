6955f3873bd172e3b2b7b5d368df66bfc6a9ada7
Tasha L <natasha.lewis.lalor@gmail.com>
2023-01-11 01:47:56 -0600

    Just comments - database is connected

--- LiftOff-Project/Models/Movie.cs
+++ LiftOff-Project/Models/Movie.cs
@@ -2,17 +2,19 @@
 {
     public class Movie
     {
-        //This ID is necessary primarily in linking a User-Watchlist-WatchListMovieId-MovieId.
+        //This model is necessary primarily in linking a User-Watchlist-WatchListMovieId-MovieId.
         //*********************************
         //
         //    If I am correct, this class will only exist for testing purposes before we integrate the API - I believe we will actually only need to store information in the
         //    WatchListMovieId objects, as A) the API can give us the movie Ids, B) the movie Ids only point to something from the API, C) We don't need a persistent store of
         //    literally just Ids on our end. Later, I will likely delete the Initial Migrations completely and start fresh when we integrate the API.
         //
+        //    Not sure yet though.
+        //
         //*********************************
         public int Id { get; set; }
 
-        //TITLE FIELD EXISTS FOR DEVELOPMENT ONLY, WILL NOT SHOW IN FULL 1.0
+        //Title field may not make it to 1.0
         public string Title { get; set; }
         public Movie () { }
 
