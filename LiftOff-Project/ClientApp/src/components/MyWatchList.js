948857dd82355a131e20625a20151e032f1c14b6
priya12845 <priya12845@gmail.com>
2023-01-11 22:05:09 -0600

    Tried pulling in API data

--- LiftOff-Project/ClientApp/src/components/MyWatchList.js
+++ LiftOff-Project/ClientApp/src/components/MyWatchList.js
@@ -1,40 +1,53 @@
-﻿import React, { useState, useEffect } from 'react';
+﻿import React, { useState, useEffect ,Component} from 'react';
 import { Table } from 'reactstrap';
-const MyWatchList = () => {
-    const [mywatchlist, setItems] = useState([]); //setting it to empty array.
-    const dataType = 1;
 
-    useEffect(() => {
-        fetch(`mywatchlist/${dataType}`)
-            .then((results) => {
-                return results.json();
+export class MyWatchList extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: [],
+            DataisLoaded: false
+        };
+    }
+    componentDidMount() {
+        fetch(
+            "https://jsonplaceholder.typicode.com/users")
+            .then((res) => res.json())
+            .then((json) => {
+                this.setState({
+                    items: json,
+                    DataisLoaded: true
+                });
             })
-            .then(data => {
-                setItems(data);
-            })
-    }, [])
-    console.log("here");
-    return (
-        <main>
-            {
-                (mywatchlist.length > 0) ? mywatchlist.map((item) =>
-                    <div>
-                    <Table striped bordered hover>
-                    <thead>
-                                <tr>
-                                    <th width="170">{item.title}</th>
-                                    <th width="170">{item.provider}</th>
-                        </tr>
-                    </thead>
-                    
-                        </Table> </div>) : <div>Loading...</div>
+    }
 
-//                (mywatchlist.length > 0) ? mywatchlist.map((item) => <div> <h3>Title : {item.title}</h3>  
-  //                  <h3>Provider :{ item.provider }</h3></div>) : <div>Loading...</div>
-            }
-        </main>
+    render() {
+        const { DataisLoaded, items } = this.state;
+        if (!DataisLoaded) return <div>
+            <h1> Pleses wait some time.... </h1> </div>;
+        else {
+            
+            
+            return (
 
-    )
+
+                <div className="App">
+                    <h1> Fetch data from an api in react </h1>  {
+                        items.map((item) => (
+                            <div>
+                                <select id="cars" name="cars">
+                                    <option value={item.username} >{item.username}</option>
+                                </select>
+                            </div>
+                        ))
+                    }
+                </div>
+
+            );
+        }
+        }
 }
 
 export default MyWatchList;
