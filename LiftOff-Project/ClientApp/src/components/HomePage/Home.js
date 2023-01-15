57d628d4f8271c3d8c75d14765d87b4a5aeb40ad
Annalyssa Stakes <annalyssastakes@Annalyssas-Air.attlocal.net>
2023-01-15 16:31:18 -0600

    started working on bottom nav bar and removed the links for Counter & Fetch Data in the top NavBar

--- LiftOff-Project/ClientApp/src/components/HomePage/Home.js
+++ LiftOff-Project/ClientApp/src/components/HomePage/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { SearchForm } from "./SearchForm";
+import { BottomNavBar } from '../BottomNavBar'
 import css from './Home.module.css';
 
 export class Home extends React.Component {
@@ -22,11 +23,7 @@ export class Home extends React.Component {
         <a href="/create-account">Sign up</a><br /><br />
 
             <SearchForm />
-
-        <div className={css.lowerleft}>
-          <img src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg" /> <br />
-          <p className={css.clearleft}>Credit: This product uses the TMDB API but is not endorsed or certified by TMDB.</p>
-        </div>
+            <BottomNavBar />
 
       </div>
     );
