ec7f0997c3853f3952f8389e22c116114fe0a87f
Tasha L <natasha.lewis.lalor@gmail.com>
2023-01-18 15:51:39 -0600

    Axios GET request and assigning the data to State is working, but parsing that data and using it in RENDER is not yet functioning

---
+++ LiftOff-Project/ClientApp/src/components/GetDataTest.js
@@ -0,0 +1,5 @@
+// import React, { useState, useEffect } from 'react';
+// import { Table } from 'reactstrap';
+// import axios from 'axios';
+
+
--- LiftOff-Project/ClientApp/src/components/SearchResultsPage/SearchTable.js
+++ LiftOff-Project/ClientApp/src/components/SearchResultsPage/SearchTable.js
@@ -1,35 +1,73 @@
 import React from 'react';
 import { Table } from 'reactstrap';
+import Axios from 'axios';
 
 export class SearchTable extends React.Component {
-    render() {
-      return (
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Movie Title</th>
-              <th>Add to Watch List?</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Movie Title</td>
-              <td><a href="">Add</a></td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Movie Title</td>
-              <td><a href="">Add</a></td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>Movie Title</td>
-              <td><a href="">Add</a></td>
-            </tr>
-          </tbody>
-        </Table>
-      );
-    }
+
+  constructor(props) {
+    super(props);
+    this.state = { posts: [] }
   }
+
+  componentDidMount() {
+    //const [Post, setPost] = React.useState(null);
+    document.body.style.background = "white";
+    const bearer = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MjcxMjdmMTRjYWNhODM5ZWY0MmQyMmEyM2RjZWZkZSIsInN1YiI6IjYzYWI5MTU3Njk5ZmI3MDBhNzU0NDEyNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wMsItq5wH6JD3RkfdsW-zCVPjOCrLjY-NcQXfkirVD4";
+    const apiUrl = "https://api.themoviedb.org/3/";
+    const searchMovies = "search/movie";
+    const andQuer = "&query=";
+    const andPage = "&page=";
+    const apiKey = "?api_key=627127f14caca839ef42d22a23dcefde";
+    const genreList = "genre/movie/list";
+    const config = {
+      headers: { Authorization: `Bearer ${bearer}` }
+    }
+    let searchString = "shrek";
+
+    Axios.all([
+      Axios.get(`${apiUrl}${searchMovies}${apiKey}${andQuer}${searchString}${andPage}1`,
+        config
+      ),
+      Axios.get(`${apiUrl}${genreList}${apiKey}`,
+        config
+      )
+    ])
+      .then((responseArr) => {
+        this.setState({ posts: [responseArr[0].data, responseArr[1].data] });
+      });
+
+  }
+
+  
+
+  render() {
+    console.log(this.state.posts[0])
+    console.log(this.state.posts[1])
+
+
+
+    return (
+      <div>
+      {this.state.posts.map(post =>
+              
+              <div>
+              <script>console.log(post)</script>
+              </div>
+            )}
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Genre</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>:(</td>
+          </tr>
+        </tbody>
+      </Table>
+      </div>
+    );
+  }
+}
