555d8e563226f2ce87ee74e9695455194474a887
Annalyssa Stakes <annalyssastakes@Annalyssas-Air.attlocal.net>
2023-01-18 20:16:36 -0600

    made changes to the search form - working on making sure the submitted data is being compiled somewhere

--- LiftOff-Project/ClientApp/src/components/HomePage/Dropdown.js
+++ LiftOff-Project/ClientApp/src/components/HomePage/Dropdown.js
@@ -1,31 +1,60 @@
 ﻿import React, { Component } from 'react';
-import { YearPicker } from 'react-dropdown-date';
+
+import { YearPicker, MonthPicker, DayPicker } from 'react-dropdown-date';
 
 export class Dropdown extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { year: null, month: null, day: null };
-    }
+  constructor(props) {
+    super(props);
+    this.state = { year: this.year, month: this.month, day: this.day };
+  }
 
-    render() {
-        return (
-            <div>
-                <YearPicker
-                    defaultValue={'select year'}
-                    start={1930}                // default is 1900
-                    reverse                     // default is ASCENDING
-                    required={true}             // default is false
-                    value={this.state.year}     // mandatory
-                    onChange={(year) => {       // mandatory
-                        this.setState({ year });
-                        console.log(year);
-                    }}
-                    id={'year'}
-                    name={'year'}
-                    classes={'classes'}
-                    optionClasses={'option classes'}
-                />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <YearPicker
+          defaultValue={'select year'}
+          start={1900}                // mandatory  
+          reverse                     // default is ASCENDING
+          value={this.state.year}     // mandatory
+          onChange={(year) => {       // mandatory
+            this.setState({ year });
+            console.log(year);
+          }}
+          id={'year'}
+          name={'year'}
+          classes={'classes'}
+          optionClasses={'option classes'}
+        />
+        <MonthPicker
+          defaultValue={'select month'}
+          short                     // default is full name
+          caps                      // default is Titlecase
+          year={this.state.year}    // mandatory
+          value={this.state.month}  // mandatory
+          onChange={(month) => {    // mandatory
+            this.setState({ month });
+            console.log(month);
+          }}
+          id={'month'}
+          name={'month'}
+          classes={'classes'}
+          optionClasses={'option classes'}
+        />
+        <DayPicker
+          defaultValue={'select day'}
+          year={this.state.year}    // mandatory
+          month={this.state.month}  // mandatory
+          value={this.state.day}    // mandatory
+          onChange={(day) => {      // mandatory
+            this.setState({ day });
+            console.log(day);
+          }}
+          id={'day'}
+          name={'day'}
+          classes={'classes'}
+          optionClasses={'option classes'}
+        />
+      </div>
+    );
+  }
 }
