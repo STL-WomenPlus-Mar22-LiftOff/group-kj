b7b0c8194aa21af424a3b20e9eb98589af9d5a8c
priya12845 <priya12845@gmail.com>
2023-01-28 18:49:21 -0600

    looking for error in the login controller

--- LiftOff-Project/ClientApp/src/components/LogIn/LogInForm.js
+++ LiftOff-Project/ClientApp/src/components/LogIn/LogInForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import axios from 'axios';
 
 class LogInForm extends React.Component {
 
@@ -17,33 +17,8 @@ class LogInForm extends React.Component {
     onSubmit = () => {
         console.log("here");
         console.log(this.refs.userid.value);
-
-        /*fetch(`login/${this.refs.userid.value}`, {
-            method: 'GET',
-            headers: { 'Content-type': 'application/json' },
-
-
-        })*/
-        fetch(`user/`).then(r => r.json()).then(res => {
-            console.log(res); //This returns array with a length > 0 or = 0
-            if (res) {
-
-                this.state.user = res;
-                if (this.state.user.length > 0) {
-                    if (this.state.user[0]['password'] === this.refs.password.value) {
-                        console.log(this.state.user[0]['password']);
-                        this.state.DataLoaded = true;
-                        alert('Login Successful');
-
-                    }
-
-                }
-                else {
-                    alert('Login Failed!!.. User Id or Password does not match.')
-                }
-
-            }
-        })
+        alert('Priyanka');
+        const res = axios.get("https://localhost:44413/login");
     }
 
     render() {
