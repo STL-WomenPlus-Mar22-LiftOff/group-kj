e062f64e33468865febb2eadc0fb5b4cd151843b
priya12845 <priya12845@gmail.com>
2023-01-29 13:57:39 -0600

    I have added both the types of components. Function component takes two clicks on the log in button and then says login successful but the class component works completely fine in one click. For now I have commented out the function component.

--- LiftOff-Project/ClientApp/src/components/NewLogIn.js
+++ LiftOff-Project/ClientApp/src/components/NewLogIn.js
@@ -1,7 +1,4 @@
-//This is function component fine that works fine when clicked the second time. I have no idea how to change this work
-// one time.
-
-/*import React, { useState } from 'react';
+﻿import React, { useState } from 'react';
 import { useForm } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
@@ -15,7 +12,7 @@ const schema = yup.object().shape({
     password: yup.string().required("Please enter your password"),
 });
 
-export function LogInForm() {
+export function NewLogIn() {
 
     const { register, handleSubmit, formState: { errors } } = useForm({
         resolver: yupResolver(schema),
@@ -29,7 +26,7 @@ export function LogInForm() {
     const [user, setUser] = useState([]);
 
     const submitLogInForm = async (data) => {
-         
+
         fetch(`login/${email}`, {
             method: 'GET',
             headers: { 'Content-type': 'application/json' },
@@ -56,13 +53,13 @@ export function LogInForm() {
             }
         })
 
-        
+
 
     };
 
     return (
         <div>
-            <form className={css.error} >
+            <form className={css.error} onSubmit={handleSubmit(submitLogInForm)}>
                 <div>
                     <input className={css.input} type="email" name="email" placeholder="Email" {...register("email", { onChange: (e) => { setEmail(e.target.value) } })} />
                     {errors.email && <p>{errors.email.message}</p>}
@@ -72,78 +69,12 @@ export function LogInForm() {
                     {errors.password && <p>{errors.password.message}</p>}
                 </div>
                 <div className="text-center">
-                    <Button variant="primary" type="submit" className={css.click} onClick={handleSubmit(submitLogInForm) }>Log In!</Button>{' '}
+                    <Button variant="primary" type="submit" className={css.click}>Log In!</Button>{' '}
                 </div>
             </form>
         </div>
     );
-}*/
-
-
-//This is the class component code working fine. It works the very first time itself.
-import React from 'react';
-
-class LogInForm extends React.Component {
-
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            message: '',
-            userid: 1,
-            user: [],
-            DataLoaded: false,
-            redirect : null
-        }
-    };
-
-       onSubmit = () => {
-
-           
-        fetch(`login/${this.refs.userid.value}`, {
-            method: 'GET',
-            headers: { 'Content-type': 'application/json' },
-
-
-        }).then(r => r.json()).then(res => {
-            console.log(res); //This returns array with a length > 0 or = 0
-            if (res) {
-
-                this.state.user = res;
-                if (this.state.user.length > 0) {
-                    if (this.state.user[0]['password'] === this.refs.password.value) {
-                        console.log(this.state.user[0]['password']);
-                        this.state.DataLoaded = true;
-                        alert('Login Successful');
-                        
-                    }
-
-                }
-                else {
-                    alert('Login Failed!!.. User Id or Password does not match.')
-                }
-
-            }
-        })
-    }
-
-    render() {
-        return (
-            <div>
-
-                <p>
-                    <label>User ID : <input type="text" ref="userid" /></label>
-                </p>
-                <p>
-                    <label>Password : <input type="text" ref="password" /></label>
-                </p >
-                <button onClick={this.onSubmit}>Login</button>
-
-            </div>
-        )
-    }
 }
-export default LogInForm;
 
 
 
