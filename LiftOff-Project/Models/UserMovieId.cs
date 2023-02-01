b6e4c3a94ce8517363db9d6aaf3ff99c262fbb36
Tasha L <natasha.lewis.lalor@gmail.com>
2023-01-31 20:05:27 -0600

    huh

---
+++ LiftOff-Project/Models/UserMovieId.cs
@@ -0,0 +1,13 @@
+﻿namespace LiftOff_Project.Models
+{
+    public class UserMovieId
+    {
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int APIMovieId { get; set; }
+
+        public UserMovieId()
+        {
+        }
+    }
+}
