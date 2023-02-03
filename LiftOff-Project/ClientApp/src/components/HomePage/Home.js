f483bcf5fd56d7b3c2b52658f3854be928c29601
Annalyssa Stakes <annalyssastakes@Annalyssas-Air.attlocal.net>
2023-02-02 20:25:32 -0600

    rebase part 12

--- LiftOff-Project/ClientApp/src/components/HomePage/Home.js
+++ LiftOff-Project/ClientApp/src/components/HomePage/Home.js
@@ -15,58 +15,6 @@ const homeBackground = {
 export class Home extends React.Component {
     static displayName = Home.name;
 
-<<<<<<< HEAD
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            isModalOpen: false,
-        };
-
-        this.handleModalOpen = this.handleModalOpen.bind(this);
-
-    }
-
-    componentDidMount() {
-        document.body.style = { homeBackground };
-    }
-
-    handleModalOpen() {
-        this.setState((prevState) => {
-            return { isModalOpen: !prevState.isModalOpen }
-        })
-    }
-
-  render() {
-    return (
-      <Container>
-        <Row>
-          <Col>
-            <div className={css.box}>
-              <h2 className={css.h2}>One search for all your streaming services. Find your next favorite movie here!</h2>
-              <ul className={css.gallery}>
-                <li><img src="https://venturebeat.com/wp-content/uploads/2016/06/netflix-logo.png?fit=750%2C750&strip=allhttps://www.google.com/url?sa=i&url=https%3A%2F%2Fventurebeat.com%2Fmedia%2Fnetflix-made-a-new-logo-thats-designed-for-mobile-devices%2F&psig=AOvVaw2QTJD9qcrMRn-De7L2ouRB&ust=1673313625263000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCNj_wJOpufwCFQAAAAAdAAAAABAE" className={css.imgsize} /></li>
-                <li><img src="https://reviewed-com-res.cloudinary.com/image/fetch/s--xT7E_lQY--/b_white,c_fill,cs_srgb,f_auto,fl_progressive.strip_profile,g_auto,h_972,q_auto,w_972/https://reviewed-production.s3.amazonaws.com/1591287416567/Hulu_Logo.jpg" className={css.imgsize} /></li>
-                <li><img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/didisney-plus-logo-1579600794.jpg" className={css.imgsize} /></li>
-                <li><img src="https://hbomax-images.warnermediacdn.com/2020-05/square%20social%20logo%20400%20x%20400_0.png" className={css.imgsize} /></li>
-                <li><img src="https://www.citypng.com/public/uploads/preview/-11596295859szf5ufbz1h.png" className={css.imgsize} /></li>
-              </ul>
-              <div>
-                            <Link to="/create-account"><Button variant="primary" className={css.click}>Sign Up!</Button>{' '}</Link>
-                            <label>Have an account?  </label><Link to=""><Button variant="primary" onClick={this.handleModalOpen} style={{ cursor: 'pointer' }}>Log in!</Button>{' '}</Link>
-                        </div>
-
-                    </div>
-
-                </Col>
-            <LogInModal
-                isModalOpen={this.state.isModalOpen}
-                handleModalOpen={this.handleModalOpen}
-            />
-                </Row>
-
-      </Container>
-=======
   constructor(props) {
     super(props);
 
@@ -118,7 +66,6 @@ export class Home extends React.Component {
           handleModalOpen={this.handleModalOpen}
         />
       </div>
->>>>>>> b6b3bbd (Updated for modal changes)
     );
   }
 }
