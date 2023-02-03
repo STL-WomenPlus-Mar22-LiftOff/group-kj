d5cc033d67232cc6cff96643576ab53e33d3507a
Annalyssa Stakes <annalyssastakes@Annalyssas-Air.attlocal.net>
2023-02-02 20:14:21 -0600

    rebase pt 6

--- LiftOff-Project/ClientApp/src/components/HomePage/SearchForm.js
+++ LiftOff-Project/ClientApp/src/components/HomePage/SearchForm.js
@@ -18,6 +18,16 @@ export function SearchBar() {
         navigate('/search-table', );
     };
 
+    const getStreamServices = async (results) => {
+        let streamServices = {};
+        for (const result of results) {
+            const response = await fetch(`https://api.themoviedb.org/3/movie/${result.id}/watch/providers?api_key=${apiKey}`);
+            const data = await response.json();
+            streamServices[result.id] = data.providers;
+        }
+        return streamServices;
+    }
+
     return (
         <form onSubmit={handleSubmit} className={css.searchbox}>
             <input
