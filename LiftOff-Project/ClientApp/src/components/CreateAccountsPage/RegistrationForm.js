edb8b2b42fbfa55d4bd57f7f61047b062a65daae
Happy <biswasnedhi09@gmail.com>
2023-02-02 20:36:27 -0600

    added hashing

--- LiftOff-Project/ClientApp/src/components/CreateAccountsPage/RegistrationForm.js
+++ LiftOff-Project/ClientApp/src/components/CreateAccountsPage/RegistrationForm.js
@@ -7,6 +7,112 @@ import css from './CreateAccount.module.css';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
+
+const schema = yup.object().shape({
+    email: yup.string().email("Please enter a valid email address").required("Please enter a valid email address"),
+    password: yup.string().required("Password is required").min(6, "Password must be at least 6 characters").max(15, "Password can only be 15 characters max"),
+    confirmPassword: yup.string().required("Confirm password is required").oneOf([yup.ref("password"), null], "Passwords must match"),
+    firstName: yup.string().required("Please enter your first name"),
+    lastName: yup.string().required("Please enter your last name"),
+});
+
+var bcrypt = require('bcryptjs');
+var salt = bcrypt.genSaltSync(10);
+
+export function RegistrationForm() {
+
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(schema),
+        mode: "onBlur",
+    });
+
+    const navigate = useNavigate();
+
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+
+    const submitRegistrationForm = async () => {
+
+        const url = `user/`;  //API controller URL
+        var hash = bcrypt.hashSync(password, salt);
+
+        let userInfo = {
+            UserName: firstName + ' ' + lastName,
+            Password: hash,
+            Email: email,
+        };
+
+
+
+        await axios.post(url, userInfo);  //this is adding the newly created user to the database
+
+        //This is fetching the newly created user's information to store in window variables to pass to the user profile page
+        fetch(`login/${email}`, {
+            method: 'GET',
+            headers: { 'Content-type': 'application/json' },
+
+        }).then(r => r.json()).then(res => {
+            if (res) {
+                if (res.length > 0) {
+                    window.user = res[0]['userName'];  // If yes, store the username in a window variable to pass to user profile page.
+                    window.userid = res[0]['id'];  //If yes, store the id in a window variable to pass to user profile page.
+                    navigate('/user-profile');
+                }
+            }
+        })
+    };
+
+    return (
+        <div>
+
+            <h1 className={css.h1}>Welcome</h1>
+            <h3 className={css.h3}>Create Your Account!</h3>
+
+            <form className={css.error} onSubmit={handleSubmit(submitRegistrationForm)}>
+                <div>
+                    <input className={css.input} type="email" name="email" placeholder="Email" {...register("email", { onChange: (e) => { setEmail(e.target.value) } })} />
+                    {errors.email && <p>{errors.email.message}</p>}
+                </div>
+                <div>
+                    <input className={css.input} type="password" name="password" placeholder="Password" {...register("password", { onChange: (e) => { setPassword(e.target.value) } })} />
+                    {errors.password && <p>{errors.password.message}</p>}
+                </div>
+                <div>
+                    <input className={css.input} type="password" name="confirmPassword" placeholder="Confirm Password" {...register("confirmPassword")} />
+                    {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
+                </div>
+                <div>
+                    <input className={css.input} type="text" name="firstName" placeholder="First Name" {...register("firstName", { onChange: (e) => { setFirstName(e.target.value) } })} />
+                    {errors.firstName && <p>{errors.firstName.message}</p>}
+                </div>
+                <div>
+                    <input className={css.input} type="text" name="lastName" placeholder="Last Name" {...register("lastName", { onChange: (e) => { setLastName(e.target.value) } })} />
+                    {errors.lastName && <p>{errors.lastName.message}</p>}
+                </div>
+                <div>
+                    <Button variant="primary" type="submit" className={css.click}>Sign Up!</Button>{' '}
+                </div>
+            </form>
+        </div>
+    );
+}
+
+
+
+
+
+/*
+import React, { useState } from 'react';
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+import Button from 'react-bootstrap/Button';
+import css from './CreateAccount.module.css';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
 const schema = yup.object().shape({
     email: yup.string().email("Please enter a valid email address").required("Please enter a valid email address"),
     password: yup.string().required("Password is required").min(6, "Password must be at least 6 characters").max(15, "Password can only be 15 characters max"),
@@ -91,3 +197,4 @@ export function RegistrationForm() {
         </div>
     );
 }
+*/
--- LiftOff-Project/ClientApp/src/components/LogIn/LogInForm.js
+++ LiftOff-Project/ClientApp/src/components/LogIn/LogInForm.js
@@ -5,6 +5,85 @@ import * as yup from "yup";
 import Button from 'react-bootstrap/Button';
 import css from './LogInForm.module.css';
 import { useNavigate } from 'react-router-dom';
+import bcrypt from "bcryptjs";
+
+const schema = yup.object().shape({
+    email: yup.string().email("Please enter your email address").required("Please enter your email address"),
+    password: yup.string().required("Please enter your password"),
+});
+
+//var bcrypt = require('bcryptjs');
+var salt = bcrypt.genSaltSync(10);
+
+export function LogInForm() {
+
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(schema),
+        mode: "onBlur",
+    });
+
+    const navigate = useNavigate(); //For navigating to the user profile page.
+
+    const [email, setEmail] = useState(""); //Setting the email entered in the textbox
+    const [password, setPassword] = useState(""); //Setting the password entered in password textbox.
+
+    const submitLogInForm = () => {
+        
+
+        fetch(`login/${email}`, {  //this is fetching from the Login API controller to pull a specific user for the email entered
+            method: 'GET',
+            headers: { 'Content-type': 'application/json' },
+
+        }).then(r => r.json()).then(res => {        
+            //let x = res.data[0][password];
+            var hash = bcrypt.hashSync(password, salt);
+            if (res) {
+                if (res.length > 0) {
+                    //This checks if the password entered matches the password for that email in the database
+                    if (bcrypt.compareSync(password, hash)) {
+                        window.user = res[0]['userName'];  // If yes, store the username in a window variable to pass to user profile page.
+                        window.userid = res[0]['id'];  //If yes, store the id in a window variable to pass to user profile page.
+                        navigate('/user-profile');
+                    } else {
+                        alert("We couldn't log you in. Please check your email and password and try again.");
+                    }
+                } else {
+                    alert("We couldn't log you in. Please check your email and password and try again.");
+                }
+            }
+        })
+    };
+
+    return (
+        <div>
+            <form className={css.error} onSubmit={handleSubmit(submitLogInForm)}>
+                <div>
+                    <input className={css.input} type="email" name="email" placeholder="Email" {...register("email", { onChange: (e) => { setEmail(e.target.value) } })} />
+                    {errors.email && <p>{errors.email.message}</p>}
+                </div>
+                <div>
+                    <input className={css.input} type="password" name="password" placeholder="Password" {...register("password", { onChange: (e) => { setPassword(e.target.value) } })} />
+                    {errors.password && <p>{errors.password.message}</p>}
+                </div>
+                <div className="text-center">
+                    <Button variant="primary" type="submit" className={css.click}>Log In!</Button>{' '}
+                </div>
+            </form>
+        </div>
+    );
+}
+
+
+
+
+/*
+import React, { useState } from 'react';
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+import Button from 'react-bootstrap/Button';
+import css from './LogInForm.module.css';
+import { useNavigate } from 'react-router-dom';
 
 const schema = yup.object().shape({
     email: yup.string().email("Please enter your email address").required("Please enter your email address"),
@@ -65,3 +144,4 @@ export function LogInForm() {
         </div>
     );
 }
+*/
