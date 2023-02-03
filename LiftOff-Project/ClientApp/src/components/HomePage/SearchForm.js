2a575482be7b39aaa9722d011dfaa6e4adbf849f
priya12845 <priya12845@gmail.com>
2023-02-03 13:45:26 -0600

    MAde Annalyssa's changes

--- LiftOff-Project/ClientApp/src/components/HomePage/SearchForm.js
+++ LiftOff-Project/ClientApp/src/components/HomePage/SearchForm.js
@@ -1,21 +1,34 @@
-﻿import { useForm } from "react-hook-form";
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from "yup";
-import Button from 'react-bootstrap/Button';
-import React from 'react';
-import ReactDOM from 'react-dom';
+﻿import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import css from './SearchForm.module.css';
+import { Table } from 'reactstrap';
 
-export class SearchForm extends React.Component {
-    render() {
-      return (
-          <form className={css.right}>
-              <label>
-                  Name:
-              <input type="text" name="name" />
-              </label>
-              <input type="submit" value="Submit" />
-          </form>
-        );
-    }
+if (!window.searchTerm) {
+    window.searchTerm = '';
 }
+export function SearchBar() {
+    const [searchTerm, setSearchTerm] = useState(window.searchTerm);
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setSearchTerm(e.target.value);
+        window.searchTerm = e.target.value;
+    };
+
+    const handleSubmit = (input) => {
+        navigate('/search-table',);
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className={css.searchbox}>
+            <input
+                className={css.inputbox}
+                type="text"
+                value={searchTerm}
+                onChange={handleChange}
+                placeholder="Search by movie title..."
+            />
+            <button type="submit">Submit</button>
+        </form>
+    );
+};
