f838aef83b841a601e82d7684d8d78abb21dcdd8
Jordan <jordan_anthony11@aol.com>
2023-02-06 16:18:13 -0600

    Updated the user profile page, converted from class component to functional component, added CSS, added buttons for my watch list and log out, and added disabled feature to form submission

--- LiftOff-Project/ClientApp/src/components/UserProfilePage/UserProfile.js
+++ LiftOff-Project/ClientApp/src/components/UserProfilePage/UserProfile.js
@@ -1,51 +1,37 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import React, { useEffect } from 'react';
 import { SearchBar } from "./SearchForm";
-import { Link } from 'react-router-dom';
 import css from './UserProfile.module.css';
+import { LogOut } from '../LogOut/LogOut';
+import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom';
 
-const homeBackground = {
-    backgroundImage: "url('./movies.jpg')",
-    backgroundSize: 'cover',
-}
+export function UserProfile() {
 
-export class UserProfile extends React.Component {
-    static displayName = UserProfile.name;
+    const navigate = useNavigate();
 
+    const onClick = () => {
+        navigate("/watch-list");
+    };
 
-    componentDidMount() {
-        document.body.style = { homeBackground };
-    }
+    useEffect(() => {
+        document.body.style.background = "rgb(1, 1, 70)";
+    }, [])
 
-    render() {
-        return (
-            <Container>
+    return (
+        <div className={css.format}>
+            <h1>Hello, {window.user}!</h1>
+            <h2 className={css.h2}>Let's find your next favorite movie.</h2>
 
-                <Row>
+            <SearchBar />
 
-                    <Col>
-                        <div className={css.box}>
-                            <h2 className={css.h2}>Hello, {window.user}!</h2>
-                            <h3 className={css.h3}>Let's find your next favorite movie!</h3>
-                        </div>
-                        <br />
-                        <br />
-                        <div className={css.box}>
-                            <SearchBar />
-                        </div>
-                    </Col>
-
-                    <Col>
-                        <div className={css.box}>
-                            <Link to="/watch-list">Check out your watch list</Link>
-                        </div>
-                    </Col>
-
-                </Row>
-
-            </Container >
-        );
-    }
+            <div className={css.rowbuttons}>
+                <div className={css.columnbuttons}>
+                    <Button variant="primary" className={css.btn} onClick={onClick}>My Watch List</Button>{' '}
+                </div>
+                <div className={css.columnbuttons}>
+                    <LogOut />
+                </div>
+            </div>
+        </div>
+    );
 }
