9ac0cf8baee3505da4010535422383cdf86188b2
Jordan <jordan_anthony11@aol.com>
2023-02-06 16:41:49 -0600

    Commented out navbar links

--- LiftOff-Project/ClientApp/src/components/NavMenu.js
+++ LiftOff-Project/ClientApp/src/components/NavMenu.js
@@ -30,7 +30,7 @@ export class NavMenu extends Component {
           <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
           <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
             <ul className="navbar-nav flex-grow">
-              <NavItem>
+              {/* <NavItem>
                 <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
               </NavItem>
               <NavItem>
@@ -44,7 +44,7 @@ export class NavMenu extends Component {
               </NavItem>
               <NavItem>
                 <NavLink tag={Link} className="text-dark" to="/watch-list">My Watch List</NavLink>
-              </NavItem>
+              </NavItem> */}
             </ul>
           </Collapse>
         </Navbar>
